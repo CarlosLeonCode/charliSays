20d26a0edff303f9e5763c28e4c4391df05cda38
Carlos Leon <carlosestebanleonpinilla@gmail.com>
2020-11-07 17:05:47 -0500

    game finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,30 +1,34 @@
 // * DOM elements 
 const startButton = document.getElementById('btn-start'),
-    mate = document.getElementById('btnManate'),
+    manate = document.getElementById('btnManate'),
     pink = document.getElementById('btnPink'),
     opal = document.getElementById('btnOpal'),
     thistle = document.getElementById('btnThistle'),
-    total_levels = 1
+    total_levels = 2,
+    score = document.getElementById('score'),
+    level = document.getElementById('level'),
+    gameZone = document.getElementById('gameZone')
 
 // * Game class 
 class Game{
 
     constructor(){
+        this.initializer = this.initializer.bind(this)
         this.initializer()
         this.generateSequence()
-        this.nextLevel()
-        this.initializer = this.initializer.bind(this)
+        // Delay for start game 
+        setTimeout(this.nextLevel, 1000)
     }
 
     // ! Functions
     initializer(){
         this.selectColor = this.selectColor.bind(this)
-        this.nextLevel = this.nextLevel.bind(this)
+        this.nextLevel = this.nextLevel.bind(this)        
         this.toggleBtnDisabled()
         this.level = 1
         this.score = 0
         this.colors = {
-            mate,
+            manate,
             pink,
             opal,
             thistle
@@ -37,19 +41,22 @@ class Game{
         this.sequence = new Array(total_levels).fill(0).map( number => Math.floor(Math.random() * 4) )
     }
 
+    // For pass of level 
     nextLevel(){
         this.illuminateSequence()
         this.addListeners()
         this.sequenceReference = 0
+        this.score = this.level == 1 ? 0 : this.level * 100
+        this.setGameDetails()
     }
 
+    // For illuminate level sequence 
     illuminateSequence(){
         for(let i = 0; i < this.level; i++){
             // Get color 
             let color = this.fromNumberToColor(this.sequence[i])
             // Illuminate color 
-            setTimeout(() => this.illuminateColor(color), 1000 * i )
-            
+            setTimeout(() => this.illuminateColor(color), 1000 * i )   
         }
         
     }
@@ -58,7 +65,7 @@ class Game{
     fromNumberToColor(number){
         switch(number){
             case 0:
-                return 'mate'
+                return 'manate'
             case 1:
                 return 'pink'
             case 2:
@@ -71,7 +78,7 @@ class Game{
     // Convertor from color to number
     fromColorToNumber(color){
         switch(color){
-            case 'mate':
+            case 'manate':
                 return 0
             case 'pink':
                 return 1
@@ -95,7 +102,7 @@ class Game{
 
     // For active onclick events over the cards 
     addListeners(){
-        this.colors.mate.addEventListener('click', this.selectColor)
+        this.colors.manate.addEventListener('click', this.selectColor)
         this.colors.opal.addEventListener('click', this.selectColor)
         this.colors.pink.addEventListener('click', this.selectColor)
         this.colors.thistle.addEventListener('click', this.selectColor)
@@ -103,54 +110,61 @@ class Game{
 
     // For inactive onclick events over the cards 
     removeListeners(){
-        this.colors.mate.removeEventListener('click', this.selectColor)
+        this.colors.manate.removeEventListener('click', this.selectColor)
         this.colors.opal.removeEventListener('click', this.selectColor)
         this.colors.pink.removeEventListener('click', this.selectColor)
         this.colors.thistle.removeEventListener('click', this.selectColor)
     }
 
+    // Select color whe user onclick
     selectColor(ev){
         let colorName = ev.target.dataset.color 
         let colorNumber = this.fromColorToNumber(colorName)
-
         this.illuminateColor(colorName)
-
+        // --
         if(colorNumber === this.sequence[this.sequenceReference]){
             this.sequenceReference ++
-
+            // If it's the card 
             if(this.sequenceReference === this.level){
                 this.level++
                 
-                
+                // * If was the last level 
                 if(this.level === (total_levels + 1)){
-                    this.removeListeners
                     this.youWin()
                 } else {
-                    // pass level 
+                // * pass level 
                     setTimeout(() => this.nextLevel(), 2000 )
                 }
             }
-
         } else {
             // you lose 
             this.youLose()
         }
     }
 
-
+    // Whe user win 
     youWin(){
         swal('Fantastic!','You win!','success')
-        .then(this.initializer)
+        .then(() => {
+            this.removeListeners()
+            this.toggleBtnDisabled()
+            this.resetGameDetails()
+            gameZone.classList.add('block')
+        })
     }
 
+    // When user lose 
     youLose(){
         swal('Oooh no!','You lose!','error')
         .then(() => {
             this.removeListeners()
-            this.initializer()
+            this.toggleBtnDisabled()
+            this.resetGameDetails()
+            gameZone.classList.add('block')
         })
     }
 
+    // Active and inactive start bottom 
     toggleBtnDisabled(){
         if(startButton.hasAttribute('disabled')){
             startButton.removeAttribute('disabled')
@@ -159,29 +173,23 @@ class Game{
         }
     }
 
+    setGameDetails(){
+        score.textContent = this.score
+        level.textContent = this.level
+    }
+
+    resetGameDetails(){
+        score.textContent = 0
+        level.textContent = 0
+    }
+
 } // Finish class
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// For start game  
 startButton.addEventListener('click',startGame)
 
 // * Game function
 function startGame(){
+    gameZone.classList.remove('block')
     window.newGame = new Game()
 }   
